24f4b77ea3780dcda2e46d0e7761e7834cee9b83
Adrien Ardra Ramadhan <adrien.ardra@ui.ac.id>
2023-05-30 13:02:45 +0700

    database

--- sbd-n14-concerthub/server/database.sql
+++ sbd-n14-concerthub/server/database.sql
@@ -34,17 +34,28 @@ CREATE TABLE ORDER_TICKET(
 CREATE TABLE KONSER(
     konser_id serial UNIQUE NOT NULL PRIMARY KEY,
     nama_konser varchar(50) NOT NULL,
-    performer_id serial UNIQUE NOT NULL,
+    performer_id serial UNIQUE,
     tanggal_perform date NOT NULL,
     kota_perform varchar(50) NOT NULL,
     venue varchar(50) NOT NULL,
     kapasitas int NOT NULL,
     kapasitas_privillege int NOT NULL,
-    review_id serial UNIQUE NOT NULL,
-    rating Value Decimal(2, 1) NOT NULL,
-    komen varchar(50),
-    review_date date NOT NULL
+    rating Decimal(2, 1) ,
+    deskripsi varchar(500)
 );
+INSERT INTO KONSER (konser_id, nama_konser, performer_id, tanggal_perform, kota_perform, venue, kapasitas, kapasitas_privillege, deskripsi)
+VALUES (1, 'Coldplay Music of The Spheres', 1, '2023-11-15', 'Jakarta', 'Gelora Bung Karno', 65000, 5000, 'Jakarta, 9 May 2023 - Coldplay have announced their hugely-anticipated return to Asia and Australia with a special run of stadium shows in November 2023, as part of their record-breaking Music Of The Spheres World Tour. The announcement marks the bands first ever show in Jakarta, taking place on 15 November at Gelora Bung Karno Stadium.');
+
+INSERT INTO KONSER (konser_id, nama_konser, performer_id, tanggal_perform, kota_perform, venue, kapasitas, kapasitas_privillege, deskripsi)
+VALUES (2, 'Bruno Major Tour of Planet Earth', 2, '2023-8-26', 'Bandung', 'Gedung Mandalawangi', 35000, 3000, 'Bruno Major Tour of Planet Earth.');
+
+INSERT INTO KONSER (konser_id, nama_konser, performer_id, tanggal_perform, kota_perform, venue, kapasitas, kapasitas_privillege, deskripsi)
+VALUES (3, 'Rex Orange Country', 3, '2024-3-21', 'Malang', 'UMM DOME', 20000, 1500, 'To celebrate one year since Rex Orange County released his fourth album WHO CARES?, he will play a one-off show at Bandung.');
+
+INSERT INTO KONSER (konser_id, nama_konser, performer_id, tanggal_perform, kota_perform, venue, kapasitas, kapasitas_privillege, deskripsi)
+VALUES (4, 'Westlife', 4, '2024-5-5', 'Surabaya', 'Balai Budaya', 40000, 4200, 'Experience the magic of Westlife as they take the stage for an unforgettable night of music and nostalgia. Immerse yourself in the captivating harmonies and timeless hits that have made Westlife one of the most beloved boy bands of all time.');
+
+
 
 CREATE TABLE PERFORMER(
     performer_id serial UNIQUE NOT NULL PRIMARY KEY,
@@ -56,7 +67,7 @@ CREATE TABLE PERFORMER(
 CREATE TABLE REVIEW(
     review_id serial UNIQUE NOT NULL PRIMARY KEY,
     konser_id int NOT NULL,
-    rating int NOT NULL,
+    rating Decimal(2, 1),
     komen Text,
     review_date date NOT NULL
 );
