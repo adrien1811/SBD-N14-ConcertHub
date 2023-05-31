c65e0762904faa46d63d5628c02398b6c66f468d
JGDoubleP <jeremy.gandase@gmail.com>
2023-05-31 14:19:02 +0700

    MainPage in Progress

---
+++ sbd-n14-concerthub/client/src/Pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Bids from '../../components/bids/Bids';
+import Header from '../../components/header/Header';
+
+
+const Home = () => {
+
+  return <div>
+   <Header />
+   <Bids title="Book Concert Now"  />
+  </div>;
+};
+
+export default Home;
