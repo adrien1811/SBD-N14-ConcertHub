65c0242dfd5c6e25a65c69b2be1e63acc9ca13e9
Naufal Febriyanto <88530267+NaufalFeb@users.noreply.github.com>
2023-06-11 12:52:10 +0700

    ORDER JALAN

--- sbd-n14-concerthub/client/src/Pages/Order/Order.jsx
+++ sbd-n14-concerthub/client/src/Pages/Order/Order.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Form from 'react-bootstrap/Form';
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "./Order.css";
 import rex from '../../assets/rsz_rex_big.jpg';
 import Coldplay from '../../assets/Coldplay.png';
@@ -9,7 +9,6 @@ import Westlife from '../../assets/rsz_weslife_big.jpg';
 
 const Order = () => {
   const { konserId } = useParams();
-  const navigate = useNavigate();
   const [concert, setConcert] = useState(null);
   const [accommodation, setAccommodation] = useState("");
   const [paymentMethod, setPaymentMethod] = useState("");
@@ -38,36 +37,60 @@ const Order = () => {
     }
   };
 
-const handlePay = () => {
-  const username = document.getElementById('username').value;
-  const phoneNumber = document.getElementById('phoneNumber').value;
-  const email = document.getElementById('email').value;
-
-  let harga_akomodasi = 0;
-  if (accommodation === 'Hotel') {
-    harga_akomodasi = 400000;
-  } else if (accommodation === 'Vila') {
-    harga_akomodasi = 600000;
-  }
-
-  let totalPayment = concert.harga_tiket + harga_akomodasi;
-
-  if (paymentMethod === 'GOPAY') {
-    totalPayment *= 0.9; // Apply 10% discount for GOPAY
-  }
-
-  const orderData = {
-    nama_pemesan: username,
-    no_telpon: phoneNumber,
-    email,
-    jenis_accomodation: accommodation,
-    jumlah_payment: totalPayment,
-    metode_pembayaran: paymentMethod,
-  };
-
-  console.log(orderData);
-  // Rest of the code remains the same
-};
+  const handlePay = async () => {
+    const username = document.getElementById("username").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+    const email = document.getElementById("email").value;
+  
+    let harga_akomodasi = 0;
+    if (accommodation === "hotel") {
+      harga_akomodasi = 400000;
+    } else if (accommodation === "vila") {
+      harga_akomodasi = 600000;
+    }
+  
+    let totalPayment = concert.harga_tiket + harga_akomodasi;
+  
+    if (paymentMethod === "GOPAY") {
+      totalPayment *= 0.9; // Apply 10% discount for GOPAY
+    }
+  
+    const orderData = {
+      nama_pemesan: username,
+      no_telpon: phoneNumber,
+      email,
+      jenis_accomodation: accommodation,
+      harga_akomodasi: harga_akomodasi,
+      metode_pembayaran: paymentMethod
+    };
+  
+    try {
+      const response = await fetch(`http://localhost:4000/konser/${konserId}/order`, {
+        method: "POST",
+        credentials: 'include',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderData),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        // Handle successful payment
+        // For example, navigate to a different page or show a success message
+        console.log('Payment successful!');
+        console.log('Order ID:', data.order_id);
+        console.log('Total Payment:', data.Total_payment);
+      } else {
+        // Handle payment error
+        // For example, display an error message to the user
+        console.error('Payment failed.');
+      }
+    } catch (error) {
+      // Handle any network or server errors
+      console.error('An error occurred during payment:', error);
+    }
+  };  
 
   const handleAccommodationChange = (event) => {
     setAccommodation(event.target.value);
@@ -115,8 +138,8 @@ const handlePay = () => {
         <div className="Accomodation">
           <Form.Select id="accommodation" aria-label="Accomodation" onChange={handleAccommodationChange}>
             <option>Accomodation</option>
-            <option value="Hotel">Hotel</option>
-            <option value="Vila">Vila</option>
+            <option value="hotel">hotel</option>
+            <option value="vila">vila</option>
           </Form.Select>
         </div>
         <div className="PaymentMethod">
@@ -128,12 +151,12 @@ const handlePay = () => {
         </div>
         <div className="Price">
           <p>Ticket Price: {concert.harga_tiket} IDR</p>
-          <p>Accomodation Price: {accommodation === "Hotel" ? "400,000" : accommodation === "Vila" ? "600,000" : "0"} IDR</p>
+          <p>Accomodation Price: {accommodation === "hotel" ? "400,000" : accommodation === "vila" ? "600,000" : "0"} IDR</p>
           <p>
-                 Payment Total: {paymentMethod === "GOPAY"
-                ? (concert.harga_tiket + (accommodation === "Hotel" ? 400000 : accommodation === "Vila" ? 600000 : 0)) * 0.9
-                : concert.harga_tiket + (accommodation === "Hotel" ? 400000 : accommodation === "Vila" ? 600000 : 0)
-           } IDR
+                Payment Total: {paymentMethod === "GOPAY"
+                ? (concert.harga_tiket + (accommodation === "hotel" ? 400000 : accommodation === "vila" ? 600000 : 0)) * 0.9
+                : concert.harga_tiket + (accommodation === "hotel" ? 400000 : accommodation === "vila" ? 600000 : 0)
+          } IDR
         </p>
         </div>
         <div className="PayButton" onClick={handlePay}>
